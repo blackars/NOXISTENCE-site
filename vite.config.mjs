f60a044778565ed2f0cc18ae1f62a4f86cdcb63d
blackars <castillo.brayan@correounivalle.edu.co>
2025-09-12 00:10:02 -0500

    target explicito para llevar a edit-tools.js al build

--- vite.config.mjs
+++ vite.config.mjs
@@ -27,6 +27,10 @@ export default defineConfig({
         {
           src: 'hojas/**/*', // Ruta de origen relativa a `root` (public/hojas)
           dest: 'hojas' // Ruta de destino relativa a `outDir` (dist/hojas)
+        },
+        { 
+          src: 'js/edit-tools.js', // Ruta de origen relativa a `root` (public/js/edit-tools.js)
+          dest: 'js' // Ruta de destino relativa a `outDir` (dist/js)
         }
       ]
     })
